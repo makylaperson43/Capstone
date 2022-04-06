49950a7221c1002da5e6c5f7dc0d1806d59e5073
Dylan Goad <dylangoad0325@gmail.com>
2022-04-06 16:07:09 -0500

    Create form, change js, styles templates, modified views

--- HUB/forms.py
+++ HUB/forms.py
@@ -1,9 +1,16 @@
 from attr import fields
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+from HUB.models import Order
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class OrderForm(forms.Form):
+    class Meta:
+        model = Order
+        fields = ['customer_name']

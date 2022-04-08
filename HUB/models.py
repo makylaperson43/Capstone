a56308ac15b53f32a53668ad48c0337861896436
Dylan Goad <dylangoad0325@gmail.com>
2022-04-08 09:23:54 -0500

    Recofig models, optimize cart file

--- HUB/models.py
+++ HUB/models.py
@@ -2,6 +2,7 @@ from unicodedata import category
 from django.db.models.deletion import CASCADE
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import CharField
 from djmoney.models.fields import MoneyField
 
 CATEGORIES = (
@@ -16,26 +17,39 @@ CATEGORIES = (
 )
 
 class Product(models.Model):
+    class Meta:
+        abstract = True
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
     category = models.TextField(choices=CATEGORIES)
-    price1 = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
-    price2 = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', null=True)
-    price3 = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', null=True)
 
     def __str__(self):
         return self.category + " - " + self.name
 
+class Coffees(Product):
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    add_in = models.CharField(max_length=30)
+
+class Food(Product):
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    add_ins = models.CharField(max_length=30)
+
+class OrderItem(models.Model):
+    coffee = models.ForeignKey(Coffees, on_delete=models.CASCADE, null=True)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
 class Order(models.Model):
     customer_name = models.CharField(max_length=200, default="")
+    items = models.ManyToManyField(OrderItem)
     date = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.customer_name + "'s order"
 
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=CASCADE, blank=True, null=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+    def allItems(self):
+        return self.items.all()
+
+    def total(self):
+        return sum([item.product.price for item in self.items.all()])

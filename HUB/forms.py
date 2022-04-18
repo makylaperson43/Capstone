cfb6dc23f6b6fa68883b03852709d9882dd66bb5
Dylan Goad <dylangoad0325@gmail.com>
2022-04-18 14:41:26 -0500

    Final form of models and forms, set up for automatic population with json data

--- HUB/forms.py
+++ HUB/forms.py
@@ -1,17 +1,15 @@
 
 from django.forms import ModelForm
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
-from HUB.models import CoffeeOrder
+from HUB.models import OrderItem, Order
 
-class CreateUserForm(UserCreationForm):
+class OrderItemForm(ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class CoffeeOrderForm(ModelForm):
-    class Meta:
-        model = CoffeeOrder
+        model = OrderItem
         fields = '__all__'
-        exclude = ['price']
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        exclude = ['user']

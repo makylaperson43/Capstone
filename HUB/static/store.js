c8fd8bb1fc5126a2e278b7d9c1f2fd3ab2673e5a
makylaperson43 <makylaperson43@gmail.com>
2022-05-04 16:30:30 -0500

    menu

--- HUB/static/store.js
+++ HUB/static/store.js
@@ -573,3 +573,25 @@ function updateCartTotal() {
   document.getElementsByClassName("cart-total-price")[0].innerText =
     "$" + total;
 }
+
+/*gallery */
+
+$(".portfolio-menu ul li").click(function () {
+  $(".portfolio-menu ul li").removeClass("active");
+  $(this).addClass("active");
+
+  var selector = $(this).attr("data-filter");
+  $(".portfolio-item").isotope({
+    filter: selector,
+  });
+  return false;
+});
+$(document).ready(function () {
+  var popup_btn = $(".popup-btn");
+  popup_btn.magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});

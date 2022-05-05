32320f22dd334b2cd7461d81e7de22003753aff8
makylaperson43 <makylaperson43@gmail.com>
2022-05-05 09:00:03 -0500

    menu touchups

--- HUB/static/store.js
+++ HUB/static/store.js
@@ -598,3 +598,10 @@ $(document).ready(function () {
     },
   });
 });
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function onClick() {
+  btn.style.backgroundColor = "salmon";
+  btn.style.color = "white";
+});

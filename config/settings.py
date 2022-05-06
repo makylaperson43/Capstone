f877a69f69312c502ed804642ea367de56838995
Dylan Goad <dylangoad0325@gmail.com>
2022-05-06 13:01:30 -0500

    new config

--- config/settings.py
+++ config/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-@vy%t!5v-6ibsf9u+r#vjooxik0+skzk^h+4h472&#kwmnd502'
+SECRET_KEY = 'django-insecure-@vy%t!5v-6ibsf9u+r#vjooxik0+skzk^h+4h472&#kwmnd502'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['thehubbistro.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
